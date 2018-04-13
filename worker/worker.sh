4dbed2284eabd744496b82941c76348768b091af
Ivan Ermilov <earthquakesan@gmail.com>
2018-04-13 13:26:06 +0200

    redirect logs to log file and stdout

--- worker/worker.sh
+++ worker/worker.sh
@@ -9,5 +9,5 @@ mkdir -p $SPARK_WORKER_LOG
 export SPARK_HOME=/spark
 
 /spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker \
-    --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
+    --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER | tee -a $SPARK_WORKER_LOG/spark-worker.out
 
